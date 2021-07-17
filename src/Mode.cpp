d214fe2bf2ab38ecafaac4866dabce98053807a0
Tiago Melo Juca <tiagomelojuca@gmail.com>
2021-07-17 20:19:18 -0300

    adjust code convention

--- src/Mode.cpp
+++ src/Mode.cpp
@@ -1,8 +1,17 @@
 #include "Mode.h"
 
 Mode::Mode(const std::string& filter, const std::string& folder)
-    : m_filter(filter), m_folder(folder)
-{}
+    : m_filter{ filter }
+    , m_folder{ folder }
+{
+    // Perhaps surprisingly, variables in the initializer list are not
+    // initialized in the order that they are specified in the
+    // initializer list. Instead, they are initialized in the order
+    // in which they are declared in the class.
+    //
+    // SOURCE:
+    // learncpp.com/cpp-tutorial/constructor-member-initializer-lists
+}
 
 const std::string& Mode::GetFilter() const
 {
