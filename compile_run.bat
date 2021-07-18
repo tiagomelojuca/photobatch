b884bafc538069ec447b1a046722729eed5a2c63
Tiago Melo Juca <tiagomelojuca@gmail.com>
2021-07-17 21:48:30 -0300

    update compile_run script and env settings file

--- compile_run.bat
+++ compile_run.bat
@@ -13,6 +13,8 @@ echo.
 photobatch ^
            --rename ^
            --folder=C:\Users\Tiago\Desktop\TMP\ccp-posts ^
-           --filter=.png
+           --filter=.png ^
+           --prefix=test_ ^
+           --snum=1
 
 echo.
