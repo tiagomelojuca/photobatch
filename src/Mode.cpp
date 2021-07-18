46d3d31efbd298c331a20ea30831b319e16eea8e
Tiago Melo Juca <tiagomelojuca@gmail.com>
2021-07-17 21:47:57 -0300

    move all mode's logic from main file to Mode class

--- src/Mode.cpp
+++ src/Mode.cpp
@@ -1,4 +1,8 @@
 #include "Mode.h"
+#include <array>
+#include <algorithm>
+#include <filesystem>
+#include "ArgumentParser.h"
 
 Mode::Mode(const std::string& filter, const std::string& folder)
     : m_filter{ filter }
@@ -27,4 +31,138 @@ void Mode::Run()
 {
     // TODO: exec time measure inside this method
     RunImpl();
+}
+
+const std::string& getInvalidChars()
+{
+    static const std::string invalidCharacters = "\\/:*?\"<>|";
+    return invalidCharacters;
+}
+
+bool hasInvalidChars(const std::string& str)
+{
+    return str.find_first_of(getInvalidChars()) != std::string::npos;
+}
+
+std::unique_ptr<Mode> createMode(const ArgumentParser& argParser)
+{
+    const bool isRenameModeOn = argParser.getFlag(Args::Flags::rename);
+    const bool isConvertModeOn = argParser.getFlag(Args::Flags::convert);
+    const bool isResizeModeOn = argParser.getFlag(Args::Flags::resize);
+    const bool isScaleModeOn = argParser.getFlag(Args::Flags::scale);
+
+    // Business Rule: user must use at least one mode, but not more than one
+    const bool modes[] = {
+        isRenameModeOn,
+        isConvertModeOn,
+        isResizeModeOn,
+        isScaleModeOn
+    };
+
+    const std::ptrdiff_t numActiveModes = std::count(std::begin(modes),
+                                                     std::end(modes),
+                                                     true);
+
+    if (numActiveModes != 1) {
+        throw std::invalid_argument(
+            "At least one mode must be active (and not more than one)"
+        );
+    }
+
+    const std::string folder = argParser.getOptionAs<const std::string>(Args::Options::folder);
+
+    if (folder.empty()) {
+        throw std::invalid_argument("Folder path not specified");
+    }
+
+    if (!std::filesystem::exists(folder)) {
+        throw std::invalid_argument("Specified folder doesn't exist");
+    }
+
+    const std::string filter = argParser.getOptionAs<const std::string>(Args::Options::filter);
+
+    if (!filter.empty() && hasInvalidChars(filter)) {
+        throw std::invalid_argument("Filter cannot contain " + getInvalidChars());
+    }
+
+    if (isResizeModeOn) {
+        int width = 0;
+        int height = 0;
+
+        try {
+            width = argParser.getOptionAs<int>(Args::Options::width);
+            height = argParser.getOptionAs<int>(Args::Options::height);
+        } catch (const std::invalid_argument&) {
+            throw std::invalid_argument("Width and height must be numbers (int)");
+        }
+
+        if (width <= 0 || height <= 0) {
+            throw std::invalid_argument("Width and height must be greater than 0");
+        }
+
+        if (filter.empty()) {
+            throw std::invalid_argument("A filter must be provided in resize mode");
+        }
+    }
+
+    if (isScaleModeOn) {
+        float amount = 0.0f;
+
+        try {
+            float amount = argParser.getOptionAs<float>(Args::Options::amount);
+        } catch (const std::invalid_argument&) {
+            throw std::invalid_argument("Amount must be a number (float)");
+        }
+
+        if (amount <= 0.0f) {
+            throw std::invalid_argument("Amount must be greater than 0");
+        }
+
+        if (filter.empty()) {
+            throw std::invalid_argument("A filter must be provided in scale mode");
+        }
+    }
+
+    if (isRenameModeOn) {
+        int startNumber = -1;
+        std::string prefix = argParser.getOptionAs<const std::string>(Args::Options::prefix);
+
+        try {
+            startNumber = argParser.getOptionAs<int>(Args::Options::startNumber);
+        } catch (const std::invalid_argument&) {
+            throw std::invalid_argument("Start number must be a number (int)");
+        }
+
+        if (startNumber < 0) {
+            throw std::invalid_argument("Start number must be greater or equal than 0");
+        }
+
+        if (prefix.empty() || hasInvalidChars(prefix)) {
+            throw std::invalid_argument("A prefix must be provided and cannot contain " + getInvalidChars());
+        }
+    }
+
+    if (isConvertModeOn) {
+        const std::string from = argParser.getOptionAs<const std::string>(Args::Options::from);
+        const std::string to = argParser.getOptionAs<const std::string>(Args::Options::to);
+        const std::array<std::string, 2> convertOptions = { "png", "jpg" };
+
+        const bool isArgFromValid = std::find(std::begin(convertOptions),
+                                              std::end(convertOptions),
+                                              from) != std::end(convertOptions);
+
+        const bool isArgToValid = std::find(std::begin(convertOptions),
+                                            std::end(convertOptions),
+                                            to) != std::end(convertOptions);
+
+        if (!isArgFromValid || !isArgToValid) {
+            throw std::invalid_argument("From/to must point to a qualified image format");
+        }
+
+        if (from == to) {
+            throw std::invalid_argument("From/to cannot be the same image format");
+        }
+    }
+
+    return nullptr;
 }
--- src/Mode.h
+++ src/Mode.h
@@ -2,6 +2,34 @@
 #define MODE_H
 
 #include <string>
+#include <memory>
+
+class ArgumentParser;
+
+namespace Args
+{
+    namespace Flags
+    {
+        static constexpr const char* help = "help";
+        static constexpr const char* rename = "rename";
+        static constexpr const char* convert = "convert";
+        static constexpr const char* resize = "resize";
+        static constexpr const char* scale = "scale";
+    }
+
+    namespace Options
+    {
+        static constexpr const char* folder = "folder";
+        static constexpr const char* filter = "filter";
+        static constexpr const char* width = "width";
+        static constexpr const char* height = "height";
+        static constexpr const char* amount = "amount";
+        static constexpr const char* prefix = "prefix";
+        static constexpr const char* startNumber = "snum";
+        static constexpr const char* from = "from";
+        static constexpr const char* to = "to";
+    }
+}
 
 class Mode
 {
@@ -22,5 +50,7 @@ private:
     std::string m_filter;
     std::string m_folder;
 };
+
+std::unique_ptr<Mode> createMode(const ArgumentParser& argParser);
 
 #endif // MODE_H
